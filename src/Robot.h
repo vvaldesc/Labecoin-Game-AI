12f96a8c998e54c8d7c748e9f854209e938b9538
sorgazb01 <sorgazb01@educarex.es>
2024-03-02 00:49:56 +0100

    clase Robot finalizada

--- src/Robot.h
+++ src/Robot.h
@@ -8,11 +8,36 @@
 #ifndef SRC_ROBOT_H_
 #define SRC_ROBOT_H_
 #include "Ficha.h"
+#include <iostream>
+#include <random>
+using namespace std;
 
-class Robot : public Ficha {
+
+const int toleranciaDificultad =3;
+
+class Robot{
+
+	string id;
+	int dificultad;
+	Ficha fr;
+
 public:
+
+	static const float dificultades [];
+
 	Robot();
+
+	int getDificultad() const;
+	void setDificultad(int dificultad);
+	const Ficha& getFr() const;
+	void setFr(const Ficha &fr);
+	const string& getId() const;
+	void setId(const string &id);
+
+	float getSleep(int toleranciaDificultad);
+
 	virtual ~Robot();
+
 };
 
 #endif /* SRC_ROBOT_H_ */
