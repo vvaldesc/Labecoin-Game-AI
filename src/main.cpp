3a7c6954d16784459fe343e63416b0dade172123
examen <examen@ExamenDAW.myguest.virtualbox.org>
2024-03-21 12:43:57 +0100

    allegro5 set compiler set linker set

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,50 @@
 #include "constants.h"
 #include "Prueba.h"
 
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+
 using namespace std;
 
 int main() {
+    
     Prueba p;
     p.saludar();
     Config config;
     config.readXml();
-    cout << "Hola mundo" << FAV_NUM << endl;
+    cout << "Hola mundoe" << FAV_NUM << endl;
+
+    // Initialize Allegro
+    al_init();
+    al_init_primitives_addon();
+    al_init_font_addon();
+    al_init_ttf_addon();
+
+    // Create a display
+    ALLEGRO_DISPLAY* display = al_create_display(800, 600);
+    if (!display) {
+        cerr << "Failed to create display!" << endl;
+        return 1;
+    }
+
+    // Main loop
+    while (true) {
+        // Clear the display
+        al_clear_to_color(al_map_rgb(0, 0, 0));
+
+        // Draw something
+        al_draw_filled_rectangle(100, 100, 200, 200, al_map_rgb(255, 0, 0));
+
+        // Flip the display
+        al_flip_display();
+    }
+
+    // Destroy the display
+    al_destroy_display(display);
+
     return 0;
 }
+
